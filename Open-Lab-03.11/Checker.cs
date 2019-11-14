f0ad7de391a41c754663eb940fd1263ee877ead2
alexhordynsky <hordynskyya@gmail.com>
2019-11-14 08:26:08 +0100

    done

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -6,7 +6,11 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            for (var i = 0; i < str.Length / 2; i++)
+                if (str[i] != str[str.Length - 1 - i])
+                    return false;
+
+            return true;
         } 
     }
 }
